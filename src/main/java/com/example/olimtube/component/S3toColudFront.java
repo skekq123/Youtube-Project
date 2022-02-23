18612bad75514e5f69d3d15419bcb7614e6f8902
고영빈 <45589210+skekq123@users.noreply.github.com>
2022-02-23 18:12:17 +0900

    CloudFront 추가
    클라이언트가 이미지나 동영상 요청 시,  S3 url에서 CloudFront의 도메인을 통해 접근하도록 변경

---
+++ src/main/java/com/example/olimtube/component/S3toColudFront.java
@@ -0,0 +1,21 @@
+package com.example.olimtube.component;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class S3toColudFront {
+
+    @Value("${cloud.aws.cloudFront.domain}")
+    public String domain;
+
+    public String changeUrl(String url){
+
+        int index = url.indexOf(".com/") + 4;
+        String res = domain + url.substring(index, url.length());
+
+        return res;
+    }
+}
