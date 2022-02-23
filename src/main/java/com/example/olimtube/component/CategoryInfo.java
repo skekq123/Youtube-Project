482f8535568fe4bd1386e123b20d06df339e8ce5
고영빈 <45589210+skekq123@users.noreply.github.com>
2022-02-23 09:47:19 +0900

    파일 크기 설정, url 오타 수정

--- src/main/java/com/example/olimtube/component/CategoryInfo.java
+++ src/main/java/com/example/olimtube/component/CategoryInfo.java
@@ -17,7 +17,7 @@ public class CategoryInfo {
         map.put(2, "https://d1jfiqncg5yujk.cloudfront.net/category/2.png");
         map.put(3, "https://d1jfiqncg5yujk.cloudfront.net/category/3.png");
         map.put(4, "https://d1jfiqncg5yujk.cloudfront.net/category/4.png");
-        map.put(5, "https://d1jfiqncg5yujk.cloudfront.netcategory/5.png");
+        map.put(5, "https://d1jfiqncg5yujk.cloudfront.net/category/5.png");
         map.put(6, "https://d1jfiqncg5yujk.cloudfront.net/category/6.png");
         map.put(7, "https://d1jfiqncg5yujk.cloudfront.net/category/7.png");
         map.put(8, "https://d1jfiqncg5yujk.cloudfront.net/category/8.png");
